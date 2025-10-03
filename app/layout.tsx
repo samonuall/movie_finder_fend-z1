b7fabb954fc7b531a38c2ca575df2904c72276be
samonuall <samonuall@gmail.com>
2025-10-03 15:39:07 -0400

    add react query and updates lint

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import { Analytics } from "@vercel/analytics/next";
+import Providers from "@/components/providers";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -17,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
-        {children}
+        <Providers>{children}</Providers>
         <Analytics />
       </body>
     </html>
